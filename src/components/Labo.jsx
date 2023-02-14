5d03f19511b916960c89d20d097406dc70ee5cae
clement-jny <82049263+clement-jny@users.noreply.github.com>
2023-02-14 22:50:46 +0100

    Design sign-up page

--- src/components/Labo.jsx
+++ src/components/Labo.jsx
@@ -39,6 +39,33 @@ export const Labo = () => {
 				<kbd className="kbd">m</kbd>
 				<kbd className="kbd">/</kbd>
 			</div>
+
+			<div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+				<div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+					<span className="countdown font-mono text-5xl">
+						<span style={{ "--value": 15 }}></span>
+					</span>
+					days
+				</div>
+				<div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+					<span className="countdown font-mono text-5xl">
+						<span style={{ "--value": 10 }}></span>
+					</span>
+					hours
+				</div>
+				<div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+					<span className="countdown font-mono text-5xl">
+						<span style={{ "--value": 24 }}></span>
+					</span>
+					min
+				</div>
+				<div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+					<span className="countdown font-mono text-5xl">
+						<span style={{ "--value": 7 }}></span>
+					</span>
+					sec
+				</div>
+			</div>
 		</>
 	);
 };
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,138 +1,87 @@
 import { useState } from "react";
 
 export const Signup = () => {
-	const [firstname, setFirstname] = useState(`Clément`);
-	const [lastname, setLastname] = useState(`Jaunay`);
-	const [email, setEmail] = useState(`c.j@mail.com`);
-	const [password, setPassword] = useState(`1234`);
-	const [passwordConfirm, setPasswordConfirm] = useState(`1234`);
-	const [birthday, setBirthday] = useState(`2001/10/25`);
-	const [gender, setGender] = useState(``);
+	const [firstname, setFirstname] = useState("Clément");
+	const [lastname, setLastname] = useState("Jaunay");
+	const [email, setEmail] = useState("c.j@mail.com");
+	const [password, setPassword] = useState("1234");
+	const [passwordConfirm, setPasswordConfirm] = useState("1234");
+	const [birthday, setBirthday] = useState("2001/10/25");
+	const [gender, setGender] = useState("");
+
+	function showPassword() {
+		console.log(".????");
+	};
 
 	return (
 		<form>
 			<h1>Inscription</h1>
 
-			<div>
-				<label htmlFor={`firstname`}>Prénom</label>
-				<input
-					id={`firstname`}
-					name={`firstname`}
-					type={`text`}
-					placeholder={`Prénom`}
-					value={firstname}
-					onChange={(e) => setFirstname(e.target.value)}
-				/>
-                
-			</div>
-
-			<div>
-				<label htmlFor={`lastname`}>Nom</label>
-				<input
-					id={`lastname`}
-					name={`lastname`}
-					type={`text`}
-					placeholder={`Nom`}
-					value={lastname}
-				/>
-			</div>
-
-			<div>
-				<label htmlFor={`email`}>Email</label>
-				<input
-					id={`email`}
-					name={`email`}
-					type={`email`}
-					placeholder={`Email`}
-					value={email}
-				/>
-			</div>
-
-			<div>
-				<label htmlFor={`password`}>Mot de passe</label>
-				<input
-					id={`password`}
-					name={`password`}
-					type={`password`}
-					placeholder={`Mot de passe`}
-					value={password}
-				/>
-			</div>
-
-			<div>
-				<label htmlFor={`passwordConfirm`}>
-					Confirmation du mot de passe
+			<div className="form-control">
+				<label className="input-group">
+					<span>Firstname</span>
+					<input name="firstname" className="input input-bordered" type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
 				</label>
-				<input
-					id={`passwordConfirm`}
-					name={`passwordConfirm`}
-					type={`password`}
-					placeholder={`Confirmation du mot de passe`}
-					value={passwordConfirm}
-				/>
 			</div>
 
+			<div className="form-control">
+				<label className="input-group">
+					<span>Lastname</span>
+					<input name="lastname" className="input input-bordered" type="text" placeholder="Nom" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+				</label>
+			</div>
+
+			<div className="form-control">
+				<label className="input-group">
+					<span>Email</span>
+					<input name="email" className="input input-bordered" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+				</label>
+			</div>
+
+			<div className="form-control">
+				<label className="input-group">
+					<span>Mot de passe</span>
+					<input name="password" className="input input-bordered" type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} />
+					<button type="button" className="btn btn-square" onClick={showPassword()}>
+						<ion-icon name="eye-outline"></ion-icon>
+					</button>
+				</label>
+			</div>
+
+			<div className="form-control">
+				<label className="input-group">
+					<span>Confirmation du mot de passe</span>
+					<input name="passwordConfirm" className="input input-bordered" type="password" placeholder="Confirmation du mot de passe" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
+					<button className="btn btn-square">
+						<ion-icon name="eye-outline"></ion-icon>
+					</button>
+				</label>
+			</div>
+
+			<div className="form-control">
+				<label className="input-group">
+					<span>Date de naissance</span>
+					<input name="birthday" className="input input-bordered" type="date" placeholder="Date de naissance" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
+				</label>
+			</div>
+
+			<div className="form-control">
+				<label className="input-group">
+					<span>Genre</span>
+					<select className="select select-bordered">
+						<option disabled selected>Select</option>
+						<option value="male">Male</option>
+						<option value="female">Female</option>
+						<option value="other">Other</option>
+					</select>
+				</label>
+			</div>
+			
 			<div>
-				<label htmlFor={`birthday`}>Date de naissance</label>
-				<input
-					id={`birthday`}
-					name={`birthday`}
-					type={`date`}
-					placeholder={`Date de naissance`}
-					value={birthday}
-				/>
+				<meter id="passwordStrength" value="24" min="0" max="100" low="25" high="75" optimum="100"></meter>
 			</div>
 
-			<div>
-				<label htmlFor={`gender`}>Genre</label>
-				<input
-					id={`gender`}
-					name={`gender`}
-					type={`radio`}
-					value={`male`}
-					onChange={(e) => setGender(e.target.value)}
-				/>{" "}
-				Homme
-				<input
-					id={`gender`}
-					name={`gender`}
-					type={`radio`}
-					value={`female`}
-					onChange={(e) => setGender(e.target.value)}
-				/>{" "}
-				Femme
-				<input
-					id={`gender`}
-					name={`gender`}
-					type={`radio`}
-					value={`other`}
-					onChange={(e) => setGender(e.target.value)}
-				/>{" "}
-				Autre
-				<select id={`gender`} name={`gender`}>
-					<option value={`male`}>Homme</option>
-					<option value={`female`}>Femme</option>
-					<option value={`other`}>Autre</option>
-				</select>
-			</div>
-
-			<div>
-				<meter
-					id={`passwordStrength`}
-					value={`24`}
-					min={`0`}
-					max={`100`}
-					low={`25`}
-					high={`75`}
-					optimum={`100`}
-				></meter>
-			</div>
-
-			<button type={`submit`} className={`btn`}>
-				S'inscrire
-			</button>
-
-			<p>{gender}</p>
+			<button type="submit" className="btn">S'inscrire</button>
 		</form>
 	);
 };
